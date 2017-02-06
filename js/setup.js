43f43561097ccfe64cc4d14f1f51069f38044214
VladoVV <wq2127@gmail.com>
2017-02-06 22:42:39 +0200

    Кексобукинг урок 4 добавил tab, открытие и закрытие через tab, закрытие через Esc, добавил события на кнопку сейв и aria-hidden тревис

--- js/setup.js
+++ js/setup.js
@@ -3,7 +3,7 @@
 var openWindow = document.querySelector('.setup-open');
 var setup = document.querySelector('.setup');
 var closeWindow = document.querySelector('.setup-close');
-var wizardCoat = document.querySelector('#wizard-coat'); //находим элемент в разметке отвечающий за накидку
+var wizardCoat = document.querySelector('#wizard-coat'); // находим элемент в разметке отвечающий за накидку
 var colorWizardCoat = [
   'rgb(101, 137, 164)',
   'rgb(241, 43, 107)',
@@ -11,24 +11,24 @@ var colorWizardCoat = [
   'rgb(56, 159, 117)',
   'rgb(215, 210, 55)',
   'rgb(0, 0, 0)'
-]; //задаем массив с возможными цветами для накидки
-var wizardEyes = document.querySelector('#wizard-eyes'); //находим элемент в разметке отвечающий за глаза 
+]; // задаем массив с возможными цветами для накидки
+var wizardEyes = document.querySelector('#wizard-eyes'); // находим элемент в разметке отвечающий за глаза
 var colorEyes = [
   'black',
   'red',
   'blue',
   'yellow',
   'green'
-]; //задаем массив с возможными цветами для глаз
-var setupFireball = document.querySelector('.setup-fireball'); //находим элемент в разметке отвечающий за фаербол
-var setupFireballWrap = document.querySelector('.setup-fireball-wrap'); //находим элемент в разметке отвечающий за цвет фаербола
+]; // задаем массив с возможными цветами для глаз
+var setupFireball = document.querySelector('.setup-fireball'); // находим элемент в разметке отвечающий за фаербол
+var setupFireballWrap = document.querySelector('.setup-fireball-wrap'); // находим элемент в разметке отвечающий за цвет фаербола
 var colorFireball = [
   '#ee4830',
   '#30a8ee',
   '#5ce6c0',
   '#e848d5',
   '#e6e848'
-]; //задаем массив с возможными цветами для фаербола
+]; // задаем массив с возможными цветами для фаербола
 var ENTER_KEYCODE = 13;
 var ESCAPE_KEYCODE = 27;
 var buttonSetupSubmit = document.querySelector('.setup-submit');
@@ -40,26 +40,26 @@ var isActivateEvent = function (event) {
 
 // Функция - Отслеживаем событие - нажатие кнопки Esc, если событие происходит закрываем окно setup (Отслеживаем событие только в setup)
 var setupKeyDownHandler = function (event) {
-    if (event.keyCode === ESCAPE_KEYCODE) {
+  if (event.keyCode === ESCAPE_KEYCODE) {
       setup.classList.add('invisible');
-    }
+  }
 };
 
-// Функция - Открываем элемент setup и отслеживаем событие (нажатие Esc - при котором окно setup закроется) 
+// Функция - Открываем элемент setup и отслеживаем событие (нажатие Esc - при котором окно setup закроется)
 var showSetupElement = function () {
   setup.classList.remove('invisible');
-  setup.setAttribute("aria-hidden", "false");
+  setup.setAttribute('aria-hidden', 'false');
   document.addEventListener('keydown', setupKeyDownHandler);
 };
 
-// Функция - Закрываем окно setup 
+// Функция - Закрываем окно setup
 var hideSetupElement = function () {
   setup.classList.add('invisible');
-  setup.setAttribute("aria-hidden", "true");
+  setup.setAttribute('aria-hidden', 'true');
   document.removeEventListener('keydown', setupKeyDownHandler);
 };
 
-//---Открываем окно---//
+// ---Открываем окно---//
 // Отслеживаем событие клик на аватарке, когда случится событие удаляем класс скрывающий объект
 openWindow.addEventListener('click', function () {
   showSetupElement();
@@ -72,7 +72,7 @@ openWindow.addEventListener('keydown', function (event) {
   }
 });
 
-//---Закрываем окно---// 
+// ---Закрываем окно---//
 // Отслеживаем событие клик на крестике, когда случится событие добавляем класс скрывающий объект
 closeWindow.addEventListener('click', function () {
   hideSetupElement();
@@ -97,7 +97,7 @@ buttonSetupSubmit.addEventListener('keydown', function (event) {
   }
 });
 
-//---изменение цвета накидки---//
+// ---изменение цвета накидки---//
 // выбираем случайный цвет из массива
 function randColorWizardCoat() {
   return colorWizardCoat[Math.floor(Math.random() * colorWizardCoat.length)];
@@ -109,8 +109,8 @@ wizardCoat.addEventListener('click', function () {
 });
 
 
-//---меняем цвет глаз волшебника---//
-//выбираем случайный цвет из массива
+// ---меняем цвет глаз волшебника---//
+// выбираем случайный цвет из массива
 function randColorEyes() {
   return colorEyes[Math.floor(Math.random() * colorEyes.length)];
 }
@@ -119,8 +119,8 @@ wizardEyes.addEventListener('click', function () {
 });
 
 
-//---меняем цвет фаербола---//
-//выбираем случайный цвет из массива
+// ---меняем цвет фаербола---//
+// выбираем случайный цвет из массива
 function randColorFireball() {
   return colorFireball[Math.floor(Math.random() * colorFireball.length)];
 }
