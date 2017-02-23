762f685c565102e18d14e333171fa50906324e11
VladoVV <wq2127@gmail.com>
2017-02-23 15:02:58 +0200

    тревис2

--- js/utils.js
+++ js/utils.js
@@ -3,11 +3,11 @@
 
   window.getRandomElementExcept = function (colors, currentColorElem) {
     var colorElement = currentColorElem;
-  
+
     var getRandomElement = function (arrColors) {
       return arrColors[Math.floor(Math.random() * arrColors.length)];
     };
-  
+
     while (colorElement === currentColorElem) {
       colorElement = getRandomElement(colors);
     }
